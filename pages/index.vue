0a1861b5697daf6eb9a3b76629a2de64deba50ed
Aymane Dara Hlamnach <dara-@outlook.jp>
2023-12-09 10:49:04 +0100

    routing & layout

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default'
+});
+
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <h1>Nuxt Routing set up successfully!</h1>
+    <p>Current route: {{ route.path }}</p>
+    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+  </div>
+</template>
+
+<style>
+  html {
+    background-color: white;
+  }
+</style>
