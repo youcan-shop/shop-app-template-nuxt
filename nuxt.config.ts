691643426d448a77b5b404a1da67d65a2c313950
Aymane Dara Hlamnach <dara-@outlook.jp>
2023-12-12 11:15:29 +0100

    qantra: client first iteration (#1)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      meta: [
+        { name: "youcan-api-key", content: process.env.YOUCAN_API_KEY },
+      ],
+    },
+  },
+});
