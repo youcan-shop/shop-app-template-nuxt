2f1e9f20c6fbac5a2ac0ba8764ad109916eb3dba
Aymane Dara Hlamnach <dara-@outlook.jp>
2023-12-18 15:01:32 +0100

    removed duplicate qantra

--- qantra/types.ts
+++
@@ -1,9 +0,0 @@
-export interface QantraMessagePayloadMap {
-    'QANTRA::SESSION_TOKEN.REQ': [],
-    'QANTRA::SESSION_TOKEN.RES': [string]
-}
-
-export type QantraMessageType = keyof QantraMessagePayloadMap;
-export type QantraMessagePayload<T extends QantraMessageType> = QantraMessagePayloadMap[T]
-export type QantraMessageEventHandler<T extends QantraMessageType> = (...params: QantraMessagePayloadMap[T]) => void
-export type QantraMessageEvent<T extends QantraMessageType> = MessageEvent<{type: QantraMessageType, payload: QantraMessagePayload<T>}>
