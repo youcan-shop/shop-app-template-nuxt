a44691ef842f60a4674e93e6049d0dd33b27d3b6
Aymane Dara Hlamnach <dara-@outlook.jp>
2023-12-18 11:16:45 +0100

    more boilerplate

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,16 @@
 <script setup lang="ts">
-const qantra = useQantra();
 
-definePageMeta({
-  layout: 'default'
-});
-
-const route = useRoute();
-
-const { data: store } = qantra.fetch('/store');
 </script>
 
 <template>
   <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
-    {{ store }}
+    Hello, I'm the app.
   </div>
 </template>
 
 <style>
 html {
   background-color: white;
+  font: var(--text-sm-regular);
 }
 </style>
---
+++ youcan/plugins/youcan-ui.ts
@@ -0,0 +1,5 @@
+import YouCanUi from '@youcan/ui-vue3';
+
+export default defineNuxtPlugin((nuxt) => {
+    nuxt.vueApp.use(YouCanUi)
+})
